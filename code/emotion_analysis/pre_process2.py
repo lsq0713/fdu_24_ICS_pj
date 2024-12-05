0083e65220065f73f84b6ddf8a8590fae777f573
tong qing <tqsun22@m.fudan.edu.cn>
2024-12-05 11:38:43 +0800

    emo_analysis and frequency_visualization

---
+++ code/emotion_analysis/pre_process2.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import re
+read_file_path = '../../data/data.csv'
+df = pd.read_csv(read_file_path)
+
+# 定义一个函数来提取中文文本
+def extract_chinese(text):
+    if isinstance(text, str):
+        # 使用正则表达式匹配中文字符
+        chinese_text = re.findall(r'[\u4e00-\u9fff]+', text)
+        # 将匹配到的中文字符拼接成字符串
+        return ''.join(chinese_text)
+    return None
+
+# 应用函数到CSV文件的文本列
+df['chinese_text'] = df['description'].apply(extract_chinese)
+df = df.dropna(subset=['chinese_text'])
+chinese_df = df[['chinese_text']]
+chinese_df = chinese_df[chinese_df['chinese_text'].apply(lambda x: isinstance(x, str) and x.strip() != '')]
+output_filename = '../../data/new_chinese_text.csv'
+chinese_df.to_csv(output_filename, index=False, encoding='utf-8-sig')
+
---
+++ code/frequency_visualization/word_pie_chart.py
@@ -0,0 +1,22 @@
+import os
+import matplotlib.pyplot as plt
+frequency_path = '../../data/word_count_output.txt'
+with open(frequency_path,'r',encoding='utf-8')as file:
+    lines = file.readlines()
+
+word_and_count = []
+for line in lines:
+    word,count = line.strip().split(': ')
+    word_and_count.append((word, int(count)))  # 将出现次数转换为整数
+
+words, counts = zip(*word_and_count)  # 解压列表为两个单独的列表
+words = words[:20]
+counts = counts[:20]
+# 绘制饼图
+plt.pie(counts, labels=words, autopct='%1.1f%%', startangle=140)
+ 
+# 添加标题
+plt.title('Word Frequency Distribution')
+ 
+# 显示图表
+plt.show()
